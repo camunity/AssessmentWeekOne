f86a64ab88fb40a5559ef9ce0e4112ae19643e2c
camunity <cameron.flowers13@gmail.com>
2015-03-13 09:13:16 -0500

    Initial Commit

---
+++ AssessmentWeekOne/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AssessmentWeekOne
+//
+//  Created by Cameron Flowers on 3/13/15.
+//  Copyright (c) 2015 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AssessmentWeekOne/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AssessmentWeekOne
+//
+//  Created by Cameron Flowers on 3/13/15.
+//  Copyright (c) 2015 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
